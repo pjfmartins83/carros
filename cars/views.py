892df466788a21ac2e01e8640b5781cb730ec026
Paulo Martins <pjfmartins83@gmail.com>
2025-09-25 17:18:30 -0300

    25/09/2025

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,7 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     CreateView,
@@ -30,26 +29,26 @@ class CarDetailView(DetailView):
     template_name = "car_detail.html"
 
 
-@method_decorator(login_required(login_url="login"), name="dispatch")
-class NewCarCreateView(CreateView):
+class NewCarCreateView(LoginRequiredMixin, CreateView):
     model = Car
     form_class = CarModelForm
     template_name = "new_car.html"
     success_url = "/cars/"
+    login_url = "login"
 
 
-@method_decorator(login_required(login_url="login"), name="dispatch")
-class CarUpdateView(UpdateView):
+class CarUpdateView(LoginRequiredMixin, UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = "car_update.html"
+    login_url = "login"
 
     def get_success_url(self):
         return reverse_lazy("car_detail", kwargs={"pk": self.object.pk})
 
 
-@method_decorator(login_required(login_url="login"), name="dispatch")
-class CarDeleteView(DeleteView):
+class CarDeleteView(LoginRequiredMixin, DeleteView):
     model = Car
     template_name = "car_delete.html"
     success_url = "/cars/"
+    login_url = "login"
